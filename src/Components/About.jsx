41a0622d6e7f5d6b77dd22499cfa0716fbd7c638
Divyam Sharma <122789885+Divyamsharma-18@users.noreply.github.com>
2024-05-14 01:23:54 +0530

    Updated some components for better UI

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -15,10 +15,10 @@ const About = () => {
   return (
     <>
       <section id='about' className='flex flex-col lg:flex-row justify-center items-center gap-10 lg:py-24 py-20 px-10 lg:px-20 bg-sky-950 w-full h-fit '>
-        <div id='left' className='lg:w-1/2 w-full flex flex-col justify-end items-center lg:items-start gap-10'>
+        <div id='left' className='lg:w-1/2 w-full flex flex-col justify-end lg:items-start gap-10'>
           <h1 data-aos="zoom-in" className='text-white text-6xl font-bold'>About 
           <span className='italic text-green-500'>Us</span></h1>
-          <h1 data-aos="zoom-in" className='text-white text-xl font-semibold text-center lg:text-start'>Welcome to DFoodApp, where culinary inspiration meets convenience!! 
+          <h1 data-aos="zoom-in" className='text-white text-xl font-semibold lg:text-start'>Welcome to DFoodApp, where culinary inspiration meets convenience!! 
           <br />
           Delicacies from the best places out there, that too at the best price range.</h1>
           <button data-aos="zoom-in" className='bg-orange-500 px-4 py-3 rounded-full hover:bg-green-500 hover:text-black font-bold mt-4'>ORDER NOW</button>
